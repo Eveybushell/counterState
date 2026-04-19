261a3f911738f1183183d2cd795df85ce2e5643e
Evelyn Bushell <155328177+Eveybushell@users.noreply.github.com>
2026-04-19 13:21:16 -0700

    Update main.jsx

--- portfolio-app/src/main.jsx
+++ portfolio-app/src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import Counter from './Counter.jsx'
 import UserProfile from './UserProfile.jsx'
+import TaskManager from './TaskManager.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <TaskManager/>
     <UserProfile/>
     <Counter />
   </StrictMode>,
